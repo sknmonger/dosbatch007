38687b77c521b2090ec9a487ce0bd208c51ce7f6
Martin Joseph Belgica <113281785+sknmonger@users.noreply.github.com>
2026-03-03 17:04:32 +0800

    Add files via upload

---
+++ playlistindos.bat
@@ -0,0 +1,97 @@
+::Playlist in Dos assisted by Gemini Ai in 30 minutes
+::No preperation
+
+echo off
+title Playlist Majos Music
+
+:menu
+
+echo ______------_____-------______-----
+echo P L A Y L I S T  M A J O S MUSIC
+echo ______------_____-------______-----
+echo Choose Song: 1-9
+echo 1.BubuKa Gangsta!?
+echo 2.Jazzy Drill
+echo 3.Ice Ice Toobig
+echo 4.Hindi na Ako Galit
+echo 5.Kamusta HipHop!?
+echo 6.Mangkukulam na Bata
+echo 7.Paldo Wey
+echo 8.VG Grammar Villagrande Papilo
+echo 9. EXIT PLAYER
+echo ______------_____-------______-----
+pause
+
+:: Input so that chosen number opens File path
+
+set /p choice="Choose a number (1-9) and press Enter:"
+
+if "%choice%"=="1" ( echo Opening track BubuKa Gangsta!?
+start "" "majosmusicplaylist\bubukaGangsta.mp3"
+pause 
+goto menu
+
+) else if "%choice%"=="2" ( 
+    
+echo Opening track jazzy n Drill
+start "" "majosmusicplaylist\JazzynDrill.mp3"
+pause 
+goto menu
+
+
+) else if "%choice%"=="3" ( 
+echo Opening track Ice Ice Toobig
+start "" "majosmusicplaylist\IceIcetubig.mp3"
+pause 
+goto menu
+
+) else if "%choice%"=="4" (
+echo Opening track Hindi na Ako Galit
+start "" "majosmusicplaylist\HindinaGalit.mp3"
+pause 
+goto menu
+
+
+) else if "%choice%"=="5" (
+echo Opening track Kamusta HipHop!?
+start "" "majosmusicplaylist\kamustahiphop32bars.mp3"
+pause 
+goto menu
+
+
+) else if "%choice%"=="6" (
+ echo Opening track Mangkukulam na Bata
+start "" "majosmusicplaylist\makukulamnabata.mp3"
+pause 
+goto menu
+
+
+) else if "%choice%"=="7" (
+ echo Opening track Paldo Wey
+start "" "majosmusicplaylist\paldowey.mp3"
+pause 
+goto menu
+
+
+ ) else if "%choice%"=="8" (
+ echo Opening track Villagrande Papilo VG Grammar
+start "" "majosmusicplaylist\vgrammarVillagrandepapilo.mp3"
+pause 
+goto menu
+
+ ) else if "%choice%"=="9" ( 
+echo Exiting the player
+pause
+exit
+
+
+) else (
+
+    echo invalid choice ... 
+    echo Choose again...
+    goto menu
+)
+
+
+
+
