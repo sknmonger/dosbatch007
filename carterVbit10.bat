ad6ea49cfa71268de589d25770d047b145af90ab
Martin Joseph Belgica <113281785+sknmonger@users.noreply.github.com>
2026-03-03 17:10:53 +0800

    Add files via upload

---
+++ carterVbit10.bat
@@ -0,0 +1,51 @@
+echo off
+
+echo song titles  
+echo this file is for the system of carter Vas for Bit 10 inclusive only. 
+echo ┌Album Tracksm Titles┐
+echo _______________________________________________
+echo ┌1.I love you dwayne┐
+ echo 2.Don't Cry
+ echo 3. Dedicate
+ echo 4.Uproar
+ echo 5.Let it Fly
+ echo 6.Can't be broken
+ echo 7.Dark Side of the Moon
+ echo 8.Mona Lisa 
+ echo 9.What about me
+ echo 10.Open Letter 
+ echo 11.Famous 
+ echo 12.Problems 
+ echo 13.Dope Niggaz
+ echo 14. Hittaz
+ echo 15. Took His Time 
+ echo 16.Open Safe
+ echo 17.Start this shit off right
+ echo 18. Demon
+ echo 19. Mess
+ echo 20. Dope New Gospel 
+ echo 21.perfect Strangers
+ echo 22.Used 2 
+ echo 23.Let it all work out
+ echo -----------------------
+ echo ┌BONUS TRACKS┐
+ echo 1.What about me?
+ echo 2.In this house
+ echo 3.Hasta La Vista 
+
+ set bit10=carterV
+ echo %bit10%
+
+
+type nul > Yourresponse.html
+type nul > EminemorLilWayne.html
+type nul > isEZmilGonnabeDat.html
+type nul > Drakeis?.html
+type nul > CashmoneyYoungMoney.html
+type nul > DuplicateUrMooolah.html
+type nul > ItsEmptyWriteSome.html
+ 
+ pause
+
+
+
---
+++ chamba.bat
@@ -0,0 +1,88 @@
+echo off
+
+color 1f
+
+echo "How to be involved in the Technorevolution?"
+Echo "What to do?"
+Echo " What if we ??"
+
+Echo "THe classic tradition starts now"
+Echo "Sound track on"
+
+
+start https://youtu.be/dQw4w9WgXcQ
+
+
+Echo "music playing"
+
+ECHO THE RABBIT HOLE!!!
+
+
+
+Color 91
+echo down
+Color 82
+echo down
+Color 73
+echo down
+Color 64
+echo down
+Color 55
+echo down
+Color 46
+echo down
+color 37
+
+
+Echo close channels + jAGRAT
+
+pause
+
+start https://youtu.be/skdE0KAFCEA
+
+
+
+color 28
+color 19
+color 01 
+
+
+
+
+
+echo INSIDE THE RABBIT HOLE
+echo Welcome to Sector 22 India Portal WHOKNOWS_THETRUTH?
+start https://maps.app.goo.gl/bgE5YLkJrh2gfPju5
+pause
+
+
+color 5B
+
+
+
+echo exit phrase "God is Good." or "Earth is Home"
+
+pause
+
+start https://youtu.be/XhZwo4ku3gU
+
+pause 
+
+
+echo "Play all sound videos at once and psychonautically trance out in sector 22.. Hello World Web3.0 downloaded"
+echo "LAST JUST SO YOU KNOW ITS ALL CHAMBA CHAMBA 
+echo "Play all videos at the same time and think sector 22 again."
+echo "Welcome to EARTH 2026 GREGORIAN CALENDAR other CALENDARDS."
+
+
+
+START https://www.google.com/search?q=world+calendars
+
+
+Pause
+
+developed by wizard1...
+
+start https://youtu.be/z5tYny7dja4
+
+:exit 
